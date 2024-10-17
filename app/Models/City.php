5c853d40b3fe315170dba4419ee02838b36a640e
dairagalceran <dairagalceran@gmail.com>
2024-10-17 20:36:11 -0300

    funcionalidad de listar, crear y editar companias

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class City extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'province_id'];
+
+
+    public function company():BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,10 +4,60 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * Class Company
+ *
+ * @property $id
+ * @property $denomination
+ * @property $cuit
+ * @property $company_name
+ * @property $sector
+ * @property $entity
+ * @property $company_category
+ * @property $scope
+ * @property $street
+ * @property $number
+ * @property $city_id
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property City $city
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Company extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['denomination','cuit','sector' , 'entity', 'company_category','scope','company_name' ,'city_id'];
+    static $rules = [
+        'denomination' => 'required|string|max:40',
+		'cuit' => 'required|Integer|max_digits:11',
+		'city_id' => 'required|integer',
+        'company_name' => 'nullable|string|max:100',
+        'sector' => 'nullable|string|max:40',
+        'entity' => 'nullable|string|max:40',
+        'company_category' => 'nullable|string|max:20',
+        'scope' => 'nullable|string|max:40',
+        'street' => 'nullable|string|max:40',
+        'number' => 'nullable|integer',
+    ];
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['denomination','cuit','company_name','sector','entity','company_category','scope','street','number','city_id'];
+
+    /**
+     * Relación 1:*
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function city():HasOne
+    {
+        return $this->hasOne(City::class , 'id', 'city_id');
+    }
+
 }
