e818896e8b4af049bfd753f0ae1dded62ef01518
juan2645 <juan2645@gmail.com>
2024-10-17 22:56:16 -0300

    Se ajusto paginacion fuera de plano.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,22 +10,16 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
    
 <body>
-
     @include('layouts.header')
-
     <div class="container-fluid">
         <div class="row">
             @include('layouts.sidebar')
-
-            <!-- Main content -->
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 @yield('content')
             </main>
         </div>
     </div>
-
     @include('layouts.footer')
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
