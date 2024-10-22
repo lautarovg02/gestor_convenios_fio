be2939ab437077a18393928dce154db8b97dab9e
Lautaro Gallo <lautarovg147@gmail.com>
2024-10-22 09:07:31 -0300

    fix: resolve issue causing test failure in task [SCRUM-303]

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -4,7 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Company;
-
+use Faker\Provider\en_US\Person; // Importa el proveedor
+use Faker\Provider\en_US\Company as FakerCompany;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
  */
@@ -17,12 +18,17 @@ class EmployeeFactory extends Factory
      */
     public function definition(): array
     {
+        $this->faker->addProvider(new Person($this->faker)); // Agrega proveedor de Person
+        $this->faker->addProvider(new FakerCompany($this->faker)); // Agrega proveedor de Company
+
+
         return [
-            'name' => $this->faker->firstName,
-            'lastname' => $this->faker->lastName,
+            'name' => $this->faker->firstName(),
+            'lastname' => $this->faker->lastName(),
             'dni' => $this->faker->unique()->numberBetween(10000000, 60000000),
-            'email' => $this->faker->unique()->safeEmail,
-            'position' => $this->faker->word,
+            'cuil' => $this->faker->unique()->numberBetween(10000000000, 60000000000),
+            'email' => $this->faker->unique()->email(),
+            'position' => $this->faker->jobTitle(),
             'is_represent' => $this->faker->boolean,
             'company_id' => Company::inRandomOrder()->first()->id, // Crea un empleado asociado a una empresa
         ];
