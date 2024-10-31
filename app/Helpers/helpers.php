0fc9c7845e913accc4ce5f82963f0653c57b75a0
Lautaro Gallo <lautarovg147@gmail.com>
2024-10-31 10:24:38 -0300

    style: update highlight color for search keywords

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -3,6 +3,6 @@
 if (!function_exists('highlightKeyword')) {
     function highlightKeyword($text, $keyword) {
         if (!$keyword) return $text; // Si no hay palabra clave, devuelve el texto original
-        return preg_replace('/(' . preg_quote($keyword, '/') . ')/i', '<span class="bg-warning">$1</span>', $text);
+        return preg_replace('/(' . preg_quote($keyword, '/') . ')/i', '<span class="bg-text-color">$1</span>', $text);
     }
 }
