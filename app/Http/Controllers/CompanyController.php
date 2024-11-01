d91505c9a5f53b75d6f575dd5ac253800f372b77
Mauricio Urban <mauricio.a.urban@gmail.com>
2024-11-01 19:46:25 -0300

    feat: implement Delete functionality

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -9,12 +9,13 @@ use App\Models\Company;
 use Exception;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
+use App\Models\Employee;
 
 
 /**
-* Class CompanyController
-* @package App\Http\Controllers
-*/
+ * Class CompanyController
+ * @package App\Http\Controllers
+ */
 class CompanyController extends Controller
 {
     /**
@@ -36,14 +37,13 @@ class CompanyController extends Controller
 
             // Obtener todas las compañías usando el modelo Company y el scope de búsqueda
             $companies = Company::search($searchTerm)->paginate(9);
+        } catch (\Exception $e) {
+            $errorMessage = 'No se pudo recuperar la información de empresas en este momento. Por favor, inténtelo más tarde.';
+            // Opcional: Puedes registrar el error para fines de depuración.
+            \Log::error('Error al obtener compañías: ' . $e->getMessage());
+        }
 
-            } catch (\Exception $e) {
-                $errorMessage = 'No se pudo recuperar la información de empresas en este momento. Por favor, inténtelo más tarde.';
-                // Opcional: Puedes registrar el error para fines de depuración.
-                \Log::error('Error al obtener compañías: ' . $e->getMessage());
-            }
-
-            return view('companies.index', compact('companies', 'searchTerm', 'errorMessage'));
+        return view('companies.index', compact('companies', 'searchTerm', 'errorMessage'));
     }
 
     /**
@@ -51,10 +51,10 @@ class CompanyController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create():View
+    public function create(): View
     {
         $cities = City::orderBy('name', 'ASC')->get();
-        return view('companies.create' , compact('cities'));
+        return view('companies.create', compact('cities'));
     }
 
     /**
@@ -63,22 +63,20 @@ class CompanyController extends Controller
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreCompanyRequest $request):RedirectResponse
+    public function store(StoreCompanyRequest $request): RedirectResponse
     {
-        try{
-            $exists = Company::where('cuit',$request->cuit)->first();
+        try {
+            $exists = Company::where('cuit', $request->cuit)->first();
 
-            if($exists) throw new Exception("Cuit duplicado");
+            if ($exists) throw new Exception("Cuit duplicado");
 
             Company::create($request->validated());
 
             return  redirect()->route('companies.index')
-                ->with('success' , 'Empresa ingresada exitosamente.');
-        }
-        catch(Exception $e){
+                ->with('success', 'Empresa ingresada exitosamente.');
+        } catch (Exception $e) {
             return redirect()->back()->withErrors(['error' => $e->getMessage()]);
         }
-
     }
 
 
@@ -88,7 +86,7 @@ class CompanyController extends Controller
      * @param  Company $company
      * @return \Illuminate\Http\Response
      */
-    public function show(Company $company):View
+    public function show(Company $company): View
     {
         $company = Company::find($company->id);
 
@@ -101,13 +99,13 @@ class CompanyController extends Controller
      * @param  Company $company
      * @return \Illuminate\Http\Response
      */
-    public function edit(Company $company):View
+    public function edit(Company $company): View
     {
         //dd($company);
         $company = Company::find($company->id);
-        $cities = City::orderBy('name' , 'ASC')->get();
+        $cities = City::orderBy('name', 'ASC')->get();
 
-        return view('companies.edit' , ['company' => $company , 'cities' => $cities]);
+        return view('companies.edit', ['company' => $company, 'cities' => $cities]);
     }
 
     /**
@@ -119,31 +117,43 @@ class CompanyController extends Controller
      */
     public function update(StoreCompanyRequest $request, Company $company)
     {
-        try{
+        try {
             dd($request);
             $exists  = Company::where('cuit', $request->cuit)
-            ->where('id', '<>', $company->id)
-            ->first();
+                ->where('id', '<>', $company->id)
+                ->first();
 
-            if($exists) throw new Exception("Cuit duplicado");
+            if ($exists) throw new Exception("Cuit duplicado");
 
             $company->update($request->validated());
-            return redirect()->route('companies.index')->with('success' , 'Empresa actualizada exitosamente.');
-        }
-        catch(Exception $e)
-        {
+            return redirect()->route('companies.index')->with('success', 'Empresa actualizada exitosamente.');
+        } catch (Exception $e) {
             return redirect()->route('companies.edit', $company->id)->withErrors(['error' => $e->getMessage()]);
         }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified company from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
      * SE DEBE ADVERTIR SOBRE LA IMPOSIBILIDAD DE HACERLO Y DAR OPCIÓN DE DESVINCULAR
      * PARA NO MOSTRARLA EN EL LISTADO
      */
-    public function destroy(Company $company)
+    public function destroy($id)
     {
-        //
-    }
+        // Encuentra la empresa por ID
+        $company = Company::findOrFail($id);
 
+        // Verifica si hay empleados relacionados
+        $employeesCount = Employee::where('company_id', $company->id)->count();
+        if ($employeesCount > 0) {
+            return response()->json(['error' => 'No se puede eliminar la empresa porque tiene empleados asociados.'], 400);
+        }
+
+        // Si no hay empleados, procede con la eliminación
+        $company->delete();
+
+        return response()->json(['success' => 'Empresa eliminada con éxito.'], 200);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,4 @@ Route::resource('/companies' , CompanyController::class);
 //CITIES
 Route::get('/cities/create' , [CityController::class , 'create' ])->name('cities.create');
 Route::post('cities', [CityController::class, 'store'])->name('cities.store');
+
