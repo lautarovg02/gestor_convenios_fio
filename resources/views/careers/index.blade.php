5802e49913cf25a434de6527c58bb8640c1b54d7
Mauricio Urban <mauricio.a.urban@gmail.com>
2024-11-15 17:05:53 -0300

    feat: add button 'Agregar Carrera'

--- resources/views/careers/index.blade.php
+++ resources/views/careers/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <a href="{{ route('careers.create') }}" class="btn btn-secondary" onclick="">
+            Agregar Carrera <i class="bi bi-plus"></i>
+        </a>
+    </div>
 
-<!-- Listado de carreras -->
+    <!-- Listado de carreras -->
 
-<h1>Aca va un listado de carreras</h1>
-
-
-
+    <h1>Aca va un listado de carreras</h1>
 @endsection
-
