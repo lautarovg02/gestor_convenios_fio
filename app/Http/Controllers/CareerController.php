577ad27f19a2fd61cc90e8d3357c3908d069f05b
Mauricio Urban <mauricio.a.urban@gmail.com>
2024-11-15 20:36:30 -0300

    feat: add validations for creating new career

--- app/Http/Controllers/CareerController.php
+++ app/Http/Controllers/CareerController.php
@@ -4,12 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Career;
 use App\Models\Department;
-use Illuminate\Console\View\Components\Alert;
 use Illuminate\Http\Request;
-use App\Models\Province;
 use App\Models\Teacher;
-use Faker\Core\Coordinates;
-
 
 class CareerController extends Controller
 {
@@ -36,13 +32,29 @@ class CareerController extends Controller
      */
     public function store(Request $request)
     {
-        $career = Career::create([
-            'name' => $request->input('name'),
-            'coordinator_id' => $request->input('coordinator_id'),
-            'department_id' => $request->input('departament_id'),
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'coordinator_id' => 'required|exists:teachers,id',
+            'departament_id' => 'required|exists:departments,id',
+        ], [
+            'name.required' => 'El nombre de la carrera es obligatorio.',
+            'coordinator_id.required' => 'Debes seleccionar un coordinador.',
+            'coordinator_id.exists' => 'El coordinador seleccionado no es válido.',
+            'departament_id.required' => 'Debes seleccionar un departamento.',
+            'departament_id.exists' => 'El departamento seleccionado no es válido.',
         ]);
 
-        // Redirigir a la vista
+        $exists = Career::where('coordinator_id', $request->coordinator_id)->exists();
+        if ($exists) {
+            return redirect()->back()->withErrors(['coordinator_id' => 'El coordinador ya está asignado a otra carrera.'])->withInput();
+        } else {
+            // Proceder a insertar el nuevo registro
+            $career = Career::create([
+                'name' => $request->input('name'),
+                'coordinator_id' => $request->input('coordinator_id'),
+                'department_id' => $request->input('departament_id'),
+            ]);
+        }
         return redirect()->route('careers.index')->with('success', 'Carrera creada exitosamente.');
     }
 
