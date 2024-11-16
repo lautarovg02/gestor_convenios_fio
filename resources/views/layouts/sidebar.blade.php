1bb5b63c2cb47231194a63283f013d3527e547cf
rom-L <larrondoromeo@gmail.com>
2024-11-16 17:22:26 -0300

    feat: add style to navLinks depending on the page the User is on

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,44 +1,45 @@
 <nav class="col-md-2 d-none d-md-block bg-light sidebar">
     <div class="position-sticky pt-3">
         <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">
+            <!-- 'request()->is() comprueba si estamos parados en la URL indicada. Si se cumple la condición, se realiza lo que está a la derecha de '?'. En cambio, si no se cumple, se hace lo que está a la derecha de ':' -->
+            <li class="nav-item rounded-pill {{request()->is('COMPLETAR*') ? 'active-nav-link' : ''}}">
+                <a class="nav-link text-body" href="#">
                     <i class="bi bi-file-earmark-text"></i>
                     Todos los convenios
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+            <li class="nav-item rounded-pill {{request()->is('COMPLETAR*') ? 'active-nav-link' : ''}}">
+                <a class="nav-link text-body" href="#">
                     <i class="bi bi-clock"></i>
                     Solicitudes pendientes
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+            <li class="nav-item rounded-pill {{request()->is('COMPLETAR*') ? 'active-nav-link' : ''}}">
+                <a class="nav-link text-body" href="#">
                     <i class="bi bi-person"></i>
                     Usuarios de secretaría
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+            <li class="nav-item rounded-pill {{request()->is('COMPLETAR*') ? 'active-nav-link' : ''}}">
+                <a class="nav-link text-body" href="#">
                     <i class="bi bi-people"></i>
                     Docentes
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+            <li class="nav-item rounded-pill {{request()->is('COMPLETAR*') ? 'active-nav-link' : ''}}">
+                <a class="nav-link text-body" href="#">
                     <i class="bi bi-journal"></i>
                     Carreras
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('companies.index')}}">
+            <li class="nav-item rounded-pill {{request()->is('companies*') ? 'active-nav-link' : ''}}">
+                <a class="nav-link text-body" href="{{route('companies.index')}}">
                     <i class="bi bi-building"></i>
                     Empresas
                 </a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+            <li class="nav-item rounded-pill {{request()->is('COMPLETAR*') ? 'active-nav-link' : ''}}">
+                <a class="nav-link text-body" href="#">
                     <i class="bi bi-mortarboard"></i>
                     Alumnos
                 </a>
