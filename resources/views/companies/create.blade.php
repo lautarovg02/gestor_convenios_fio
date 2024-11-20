c21843d4c58d0bfa52e812c35739d1899618cd0f
dairagalceran <dairagalceran@gmail.com>
2024-11-20 10:07:03 -0300

    refactor: create and edit companies chang inputs for selects with other options and radios

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -33,34 +33,35 @@
                 </div>
             @endif
 
-
-
             <div class="card-body">
                 <form method="POST" action=" {{route('companies.store')}} " id="" role="form">
                     @csrf
+                    <!-- Campo denominación -->
                     <div class="form-group mb-3">
                         <label class="form-label">
                             <label for="denomination" class="required-field fs-6">Denominación</label>
                         </label>
                         <div>
-                            <input class="form-control fs-6" placeholder="Denominación" name="denomination" type="text" id="denomination">
+                            <input class="form-control fs-6" placeholder="Denominación" name="denomination" type="text" id="denomination" value="{{ old('denomination') }}">
                         </div>
                         @error('denomination')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
+                    <!-- Campo cuit -->
                     <div class="form-group mb-3">
                         <label class="form-label  fs-6 required-field">
                             <label for="cuit">Cuit</label>
                         </label>
                         <div>
-                            <input class="form-control fs-6" placeholder="Cuit" name="cuit" type="text" id="cuit">
+                            <input class="form-control fs-6" placeholder="Cuit" name="cuit" type="text" id="cuit" value="{{ old('cuit') }}">
                             <small class="form-hint">Ingresar <b>CUIT</b> sin guiones.</small>
                             @error('cuit')
                                 <div class="text-danger">{{$message}}</div>
                             @enderror
                         </div>
                     </div>
+                    <!-- Campo nombre de la empresa -->
                     <div class="form-group mb-3">
                         <label class="form-label fs-6">
                             <label for="company_name">Nombre de la empresa</label>
@@ -74,45 +75,73 @@
                             <label for="sector">Sector</label>
                         </label>
                         <div>
-                            <input class="form-control" placeholder="Sector" name="sector" type="text" id="sector">
+                            <input class="form-control" placeholder="Sector" name="sector" type="text" id="sector" value="{{old('sector')}}">
                         </div>
                     </div>
+                    <!-- Selector de entidad con opción "Otros" -->
                     <div class="form-group mb-3">
                         <label class="form-label fs-6">
                             <label for="entity">Entidad</label>
                         </label>
-                        <div>
-                            <input class="form-control" placeholder="Entidad" name="entity" type="text" id="entity">
+                        <select name="entity" id="entity" class="form-select">
+                            <option value="">Seleccionar</option>
+                            @foreach ($entityTypes as $type )
+                                <option value="{{$type}}" {{old('entity') == $type ? 'selected' : ''}}>{{$type}}</option>
+                            @endforeach
+                            <option value="other">Otro tipo</option>
+                        </select>
+                        <!-- Campo de texto oculto para ingresar otra opción -->
+                        <div id="otherEntityInputWrapper" class="mt-2" style="display:none">
+                            <label for="other_entity_input">Especificar otra opción:</label>
+                            <input type="text" id="other_entity_input" name="other_entity_input" style="display:none" placeholder="Nueva entidad">
+                            <input type="hidden" name="other_entity" id="other_entity">
                         </div>
                     </div>
+                    <!-- Campo rubro -->
                     <div class="form-group mb-3">
                         <label class="form-label fs-6">   <label for="company_category">Categoría</label></label>
                         <div>
-                            <input class="form-control" placeholder="Categoría" name="company_category" type="text" id="company_category">
+                            <input class="form-control" placeholder="Categoría" name="company_category" type="text" id="company_category" value="{{old('company_category')}}">
                         </div>
                     </div>
+                    <!-- Campo Ámbito con varias opciones -->
                     <div class="form-group mb-3">
-                        <label class="form-label fs-6">   <label for="scope">Ámbito</label></label>
-                        <div>
-                            <input class="form-control" placeholder="Ámbito" name="scope" type="text" id="scope">
+                        <label class="form-label fs-6" for="scope">Ámbito</label>
+                        <div style="display: flex; align-items: center; gap: 10px;">
+                            <div>
+                                <input class="form-check-input" type="radio" id="scope_1" name="scope" value="Nacional" {{ old('scope', 'NACIONAL') == 'NACIONAL' ? 'checked' : '' }}>
+                                <label class="form-check-label" for="scope_1">
+                                    Nacional
+                                </label>
+                            </div>
+                            <div>
+                                <input class="form-check-input" type="radio" id="scope_2" name="scope" value="Internacional" {{ old('scope') == 'INTERNACIONAL' ? 'checked' : '' }}>
+                                <label class="form-check-label" for="scope_2">
+                                    Internacional
+                                </label>
+                            </div>
                         </div>
                     </div>
+
+                    <!-- Campo nombre de la calle -->
                     <div class="form-group mb-3">
                         <label class="form-label fs-6">
                             <label for="street">Calle</label>
                         </label>
                         <div>
-                        <input class="form-control" placeholder="Street" name="street" type="text" id="street">
+                        <input class="form-control" placeholder="Street" name="street" type="text" id="street" value="{{old('street')}}">
                         </div>
                     </div>
+                    <!-- Campo número -->
                     <div class="form-group mb-3">
                         <label class="form-label fs-6">
                             <label for="number">Número</label>
                         </label>
                         <div>
-                            <input class="form-control" placeholder="Number" name="number" type="text" id="number">
+                            <input class="form-control" placeholder="Number" name="number" type="text" id="number" value="{{old('number')}}">
                         </div>
                     </div>
+                    <!-- Campo nombre de la ciudad -->
                     <div class="form-group mb-3">
                         <label class="form-label fs-6 required-field">
                             <label for="city_id">Ciudad</label>
@@ -131,6 +160,7 @@
                         </div>
                     </div>
 
+                    <!-- Botones -->
                     <div class="form-footer">
                         <div class="text-end">
                             <div class="d-flex">
