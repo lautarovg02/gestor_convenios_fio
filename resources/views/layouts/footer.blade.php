bc9fed3444634cdc3b7c1aa802fb1c1c1899ed15
juan2645 <juan2645@gmail.com>
2024-12-01 21:41:00 -0300

    Change City in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-light py-3 mt-1 border-top">
     <div class="container text-center mt-5 pt-5 mb-5 pb-5">
-        TUDAI Olavarrá 2024
+        TUDAI Olavarría 2024
     </div>
 </footer>
