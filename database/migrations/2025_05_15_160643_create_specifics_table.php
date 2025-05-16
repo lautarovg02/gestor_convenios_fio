873dd836fadfc1d57fee8ffce7acb020e07235cc
agusrypstra <agusrypstra@gmail.com>
2025-05-16 18:19:37 -0300

    feat: Specific model, controller and factory creates

---
+++ database/migrations/2025_05_15_160643_create_specifics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('specifics', function (Blueprint $table) {
+        $table->unsignedBigInteger('contract_id');
+        $table->foreign('contract_id')->references('id')->on('contracts')->onDelete('cascade');
+        
+        $table->date('signing_date')->nullable();
+        $table->string('objective', 200);
+        $table->text('commitment_parties');
+        $table->string('responsable_control_company', 100)->nullable();
+        $table->string('responsable_control_fio', 100)->nullable();
+        $table->binary('file');
+
+        $table->timestamps();
+
+    });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('specifics');
+    }
+};
