9a63645fa50a549e120e599eed02acb0659799ba
agusrypstra <agusrypstra@gmail.com>
2025-05-17 13:44:27 -0300

    feat: model agregado

---
+++ app/Models/SpecificResidenceAgreement.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SpecificResidenceAgreement extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'internship_initial_date',
+        'task',
+        'signing_date',
+        'contract_id',
+        'student_id',
+        'file',
+    ];
+
+    public function contract()
+    {
+        return $this->belongsTo(Contract::class);
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
