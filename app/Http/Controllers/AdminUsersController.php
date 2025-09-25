3ebacaf92eacc81265d8206cceccbce83f8e47bf
Leandro Ramos <leandro.97.ramos@gmail.com>
2025-09-25 18:22:13 -0300

    Unifico usuarios para ver gestion de usuarios y agrego rutas

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -15,14 +15,23 @@ class AdminUsersController extends Controller
     {
         $this->serviceUsers = $serviceUsers;
     }
-    
-public function index()
-{
-    $teachers = $this->serviceUsers->getAllTeachers();
-    $secretaries = $this->serviceUsers->getAllSecretaries();
 
-    return view('adminUsers.index', compact('teachers', 'secretaries'));
-}
+    public function index()
+    {
+        //   $teachers = $this->serviceUsers->getAllTeachers(); //Obtengo todos los teachers
+        // $secretaries = $this->serviceUsers->getAllSecretaries(); // Obtengo todos los secretaries
+        $users = User::select('id', 'email', 'role_id')
+            ->with([
+                'role:id,name',
+                'teacher:id,user_id,name,lastname',
+                'secretary:id,user_id,username'
+            ])
+            ->orderByDesc('id')
+            ->get();
+
+
+        return view('adminUsers.index', compact('users'));
+    }
 
     public function create()
     {
@@ -41,7 +50,24 @@ public function index()
 
     public function destroy($id)
     {
-        // Lógica para eliminar un usuario
-        return redirect()->route('adminUsers.index')->with('success', 'Usuario eliminado correctamente');
+        $user = User::findOrFail($id);
+
+        // Guardarraíles
+        if (auth()->id() === $user->id) {
+            return back()->with('error', 'No podés eliminarte a vos mismo.');
+        }
+
+        // Evitar borrar al último admin (ajustá según tu modelo de rol)
+        if ($user->rol?->name === 'admin') {
+            $remaining = User::whereHas('rol', fn($q) => $q->where('name', 'admin'))
+                ->where('id', '!=', $user->id)
+                ->count();
+            if ($remaining === 0) {
+                return back()->with('error', 'No se puede eliminar al último administrador.');
+            }
+        }
+
+        $user->delete(); // dispara ON DELETE CASCADE en secretaries/teachers
+        return back()->with('success', 'Usuario eliminado correctamente.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,95 +29,115 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/', fn() => auth()->check() ? redirect('/home') : redirect('/login'));
 
 
+
 //agrupa todas las rutas que requieran autenticación
 Route::group(['middleware' => ['role:secretary|admin|teacher']], function () {
 
 
-//ADMIN USERS
-Route::resource('adminUsers', AdminUsersController::class);
+    //ADMIN USERS
+    Route::resource('adminUsers', AdminUsersController::class);
 
-// HOME
-Route::resource('home', HomeController::class);
+    Route::delete('/admin/users/{user}', [AdminUsersController::class, 'destroy'])
+        ->name('adminUsers.destroy')
+        ->middleware(['auth', 'role:admin']);
 
-// COMPANIES
-Route::resource('companies', CompanyController::class);
+    //ADMIN USERS
+    Route::get('/admin/users', [AdminUsersController::class, 'index'])
+        ->name('adminUsers.index');
 
-// EMPLOYEES
-Route::resource('companies.employees', EmployeeController::class)->shallow();
+    Route::delete('/admin/users/{user}', [AdminUsersController::class, 'destroy'])
+        ->name('adminUsers.destroy')
+        ->middleware(['auth', 'role:admin']);
 
-//CITIES
-
-Route::get('/cities', [CityController::class, 'getCiudades'])->name('get.ciudades');
-Route::get('/cities/create', [CityController::class, 'create'])->name('cities.create');
-Route::post('/cities', [CityController::class, 'store'])->name('cities.store');
-
-//TEACHERS
-Route::resource('/teachers', TeacherController::class);
-
-//CAREERS
-Route::resource('/careers', CareerController::class);
-
-//DEPARTMENTS
-route::resource('/departments', DepartmentController::class);
-Route::delete('/departments/{department}', [DepartmentController::class, 'destroy'])->name('departments.destroy');
-
-// AGREEMENTS
-Route::resource('/agreements', AgreementController::class)->name('index', 'agreements.index');
-Route::get('/frameworkAgreement/download', [FrameworkAgreementController::class, 'download'])->name('agreement.download');
-
-Route::resource('frameworkAgreement', FrameworkAgreementController::class);
-
-Route::get('/frameworkInternshipAgreement/download', [FrameworkInternshipAgreementController::class, 'download'])->name('agreement.download');
-
-Route::resource('frameworkInternshipAgreement', FrameworkInternshipAgreementController::class);
-
-Route::resource('frameworkResidenceAgreement', FrameworkResidenceAgreementController::class);
-
-
-
-Route::resource('specificResidenceAgreement', SpecificResidenceAgreementController::class);
+    Route::get('/admin/users/{user}/edit',  [AdminUsersController::class, 'edit'])
+        ->name('adminUsers.edit');
 
 
 
 
-Route::resource('frameworkInternshipAgreement', FrameworkInternshipAgreementController::class);
-
-Route::resource('frameworkResidenceAgreement', FrameworkResidenceAgreementController::class);
-
-Route::get('/buscarConvenio/{company}', [FrameworkResidenceAgreementController::class, 'searchAgreementByCompany']);
-
-Route::resource('specificResidenceAgreement', SpecificResidenceAgreementController::class);
 
 
-Route::resource('specificAgreement', SpecificAgreementController::class);
-Route::get('/specificAgreement/getFrameworkData/{id}', [SpecificAgreementController::class, 'getFrameworkAgreementData']);
 
 
-// AJAX Routes
-Route::get('/api/company/{id}', [CompanyController::class, 'getCompanyById']);
-Route::get('/api/employees/{companyId}', [EmployeeController::class, 'getEmployeesByCompany']);
-Route::get('/api/buscarConvenio/{company}', [FrameworkResidenceAgreementController::class, 'searchAgreementByCompany']);
-Route::get('/api/employee/{id}', [EmployeeController::class, 'getEmployeeById']);
-Route::get('/api/student/{dni}', [StudentController::class, 'getStudentByDni']);
-// CONVENIOS DE PASANTIA INDIVIDUALES
-// Acción extra para seleccionar empresa y alumno (POST)
-Route::post('individual-internship-agreements/select-company', [IndividualInternshipAgreementController::class, 'selectCompany'])
-    ->name('individual-internship-agreements.select-company');
+    // HOME
+    Route::resource('home', HomeController::class);
+
+    // COMPANIES
+    Route::resource('companies', CompanyController::class);
+
+    // EMPLOYEES
+    Route::resource('companies.employees', EmployeeController::class)->shallow();
+
+    //CITIES
+
+    Route::get('/cities', [CityController::class, 'getCiudades'])->name('get.ciudades');
+    Route::get('/cities/create', [CityController::class, 'create'])->name('cities.create');
+    Route::post('/cities', [CityController::class, 'store'])->name('cities.store');
+
+    //TEACHERS
+    Route::resource('/teachers', TeacherController::class);
+
+    //CAREERS
+    Route::resource('/careers', CareerController::class);
+
+    //DEPARTMENTS
+    route::resource('/departments', DepartmentController::class);
+    Route::delete('/departments/{department}', [DepartmentController::class, 'destroy'])->name('departments.destroy');
+
+    // AGREEMENTS
+    Route::resource('/agreements', AgreementController::class)->name('index', 'agreements.index');
+    Route::get('/frameworkAgreement/download', [FrameworkAgreementController::class, 'download'])->name('agreement.download');
+
+    Route::resource('frameworkAgreement', FrameworkAgreementController::class);
+
+    Route::get('/frameworkInternshipAgreement/download', [FrameworkInternshipAgreementController::class, 'download'])->name('agreement.download');
+
+    Route::resource('frameworkInternshipAgreement', FrameworkInternshipAgreementController::class);
+
+    Route::resource('frameworkResidenceAgreement', FrameworkResidenceAgreementController::class);
+
+
+
+    Route::resource('specificResidenceAgreement', SpecificResidenceAgreementController::class);
+
+
+
+
+    Route::resource('frameworkInternshipAgreement', FrameworkInternshipAgreementController::class);
+
+    Route::resource('frameworkResidenceAgreement', FrameworkResidenceAgreementController::class);
+
+    Route::get('/buscarConvenio/{company}', [FrameworkResidenceAgreementController::class, 'searchAgreementByCompany']);
+
+    Route::resource('specificResidenceAgreement', SpecificResidenceAgreementController::class);
+
+
+    Route::resource('specificAgreement', SpecificAgreementController::class);
+    Route::get('/specificAgreement/getFrameworkData/{id}', [SpecificAgreementController::class, 'getFrameworkAgreementData']);
+
+
+    // AJAX Routes
+    Route::get('/api/company/{id}', [CompanyController::class, 'getCompanyById']);
+    Route::get('/api/employees/{companyId}', [EmployeeController::class, 'getEmployeesByCompany']);
+    Route::get('/api/buscarConvenio/{company}', [FrameworkResidenceAgreementController::class, 'searchAgreementByCompany']);
+    Route::get('/api/employee/{id}', [EmployeeController::class, 'getEmployeeById']);
+    Route::get('/api/student/{dni}', [StudentController::class, 'getStudentByDni']);
+    // CONVENIOS DE PASANTIA INDIVIDUALES
+    // Acción extra para seleccionar empresa y alumno (POST)
+    Route::post('individual-internship-agreements/select-company', [IndividualInternshipAgreementController::class, 'selectCompany'])
+        ->name('individual-internship-agreements.select-company');
 
     Route::get('individual-internship-agreements/fill-form', [IndividualInternshipAgreementController::class, 'fillForm'])
-    ->name('individual-internship-agreements.fill-form');
+        ->name('individual-internship-agreements.fill-form');
 
-// Descargar archivo del convenio
-Route::get('individual-internship-agreements/{id}/download', [IndividualInternshipAgreementController::class, 'download'])
-    ->name('individual-internship-agreements.download');
+    // Descargar archivo del convenio
+    Route::get('individual-internship-agreements/{id}/download', [IndividualInternshipAgreementController::class, 'download'])
+        ->name('individual-internship-agreements.download');
 
-Route::resource('individual-internship-agreements', IndividualInternshipAgreementController::class);
+    Route::resource('individual-internship-agreements', IndividualInternshipAgreementController::class);
 
-// Buscar alumnos (para AJAX)
-Route::get('/students/search', [StudentController::class, 'search'])->name('students.search');
-//Ruta alumnos
-Route::resource('students', StudentController::class);
-
-
-
+    // Buscar alumnos (para AJAX)
+    Route::get('/students/search', [StudentController::class, 'search'])->name('students.search');
+    //Ruta alumnos
+    Route::resource('students', StudentController::class);
 });
