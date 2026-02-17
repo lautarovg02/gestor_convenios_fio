ebdc2ecc0cec1663a0754811c10bb13ec8def2fa
Gianluca Ceschinelli <ceschinelligianluca@gmail.com>
2026-02-17 17:06:09 -0300

    agregue relacion con user en modelo de rejection para poder utilizarlo luego si asi lo requerimos

--- app/Models/ContractRejection.php
+++ app/Models/ContractRejection.php
@@ -15,4 +15,9 @@ class ContractRejection extends Model
     {
         return $this->belongsTo(Contract::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
